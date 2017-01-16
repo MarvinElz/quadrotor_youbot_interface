c8f37d38e15928e9e0d45e957534ebcb7a16f267
MarvinElz <MarvinElz@gmx.de>
2017-01-16 13:01:50 +0100

    added message support for youbot-commands

--- src/Interface.cpp
+++ src/Interface.cpp
@@ -1,12 +1,25 @@
 #include "ros/ros.h"
-
+#include "brics_actuator/JointPositions.h"
+#include "geometry_msgs/Twist.h"
 #include "quadrotor_control/kinematics.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
 
 double scale = 1.0;
 double a1, a2, a3;
 double d1, d5;
 double k_Arm, k_Ell;
 
+ros::Publisher pub_joint;
+ros::Publisher pub_base;
+
+std::string to_string(int i){
+    char number_buffer[10];
+    snprintf(number_buffer, sizeof(number_buffer), "%d", i);
+    std::string number( number_buffer );
+    return number;
+}
 
 void callback_kin_model( const quadrotor_control::kinematics::Ptr& msg ){
 	// Scale: Vx, Vy, Vz
@@ -30,20 +43,49 @@ void callback_kin_model( const quadrotor_control::kinematics::Ptr& msg ){
 	//ROS_INFO( "a: %f", a );
 	double b = acos( (x4*x4 + y4*y4 + a2*a2 - a3*a3)/(2*a2*sqrt(x4*x4+y4*y4)) );
 	//ROS_INFO( "b: %f", b );
+	double alpha = atan2( sin(msg->pose.position.x), -cos(msg->pose.position.x)*sin(msg->pose.position.y) );
+	double gamma = asin( cos(msg->pose.position.y)*cos(msg->pose.position.x) );
 
-	g[0] = M_PI*(k_Arm-1)/(2);
+	g[0] = gamma + M_PI*(k_Arm-1)/(2);
 	g[1] = atan2(y4, x4) + k_Arm*k_Ell * b - M_PI/2;
 	g[2] = k_Arm * k_Ell * (a-M_PI);
 	g[3] = k_Arm * ( msg->pose.orientation.x ) -g[1] - g[2];
-	g[4] = msg->pose.orientation.z + (k_Arm -1)/(2)*M_PI;
+	g[4] = msg->pose.orientation.z - gamma + (k_Arm -1)/(2)*M_PI;
 	
 	ROS_INFO( "Gelenkwinkel: %f, %f, %f, %f, %f", g[0],g[1],g[2],g[3],g[4] );	
 	ROS_INFO( "Winkelsumme: %f", g[0]+g[1]+g[2]+g[3] );	
 
-	// TODO: Vx, Vy direkt an Plattform senden
-	// TODO: Psi direkt an Gelenk 5 senden
-	// TODO: (Phi/Theta) und Z verrechnen
-	
+    brics_actuator::JointPositions msg_joints;
+    geometry_msgs::Twist msg_base;
+
+    msg_base.linear.x = msg->vel.linear.x;
+    msg_base.linear.y = msg->vel.linear.y;
+    msg_base.linear.z = 0.0f;
+
+    msg_base.angular.x = 0.0f;
+    msg_base.angular.y = 0.0f;
+    msg_base.angular.z = 0.0f;
+
+    pub_base.publish(msg_base);
+    
+
+    ros::Time time_now = ros::Time::now();
+    for( int i = 0; i <= 4; i++ ){
+        brics_actuator::JointValue jv;
+        jv.timeStamp = time_now;        
+        jv.joint_uri = "arm_joint_" + to_string(i+1);
+        jv.unit = "rad";
+        jv.value = g[i];
+        msg_joints.positions.push_back(jv);
+    }
+        
+    brics_actuator::Poison poison;
+    poison.originator   = "";   // what?
+    poison.description  = "";   // what?
+    poison.qos          = 1.0f; // right?
+
+    msg_joints.poisonStamp = poison;
+	pub_joint.publish( msg_joints );
 }
 
 // TODO: Skaliere Vx, Vy, Vz
@@ -77,6 +119,9 @@ int main(int argc, char **argv)
 	// for testing
 	ros::Subscriber sub_kin = nh.subscribe("/kin_measure", 10, callback_kin_model);
 	//ros::Subscriber sub_kin = nh.subscribe("/kin_model", 10, callback_kin_model);
+
+    pub_joint = nh.advertise<brics_actuator::JointPositions>("/arm_1/arm_controller/position_command", 100);
+    pub_base  = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 100);
 		
 	ros::spin();
 	return 0;
