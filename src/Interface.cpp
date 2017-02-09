487efc84beac2d59a466613a7e0e17b1d8aea359
Marvin Elz <MarvinElz@gmx.de>
2017-02-09 23:23:44 +0100

    added safe-State for collision avoidance + memcpy for last kin_model

--- src/Interface.cpp
+++ src/Interface.cpp
@@ -15,10 +15,16 @@
 #include "quadrotor_control/kinematics.h"
 
 #include <string>
+#include <cstring>
 
 #include <Eigen/Dense>
 #include <kdl/jntarray.hpp>
 #include <kdl/frames.hpp>
+
+// für Umrechnung B -> N
+#include <tf/LinearMath/Matrix3x3.h>
+#include <tf/LinearMath/Vector3.h>
+
 
 // Skalierung der Translationsgeschwindigkeit & Raum
 double scaleV = 0.15;
@@ -30,7 +36,6 @@ double a1, a2, a3;
 double d1, d5;
 double k_Arm, k_Ell;
 
-// const double my_joint_offsets[5] = {2.9496, 1.13446, -2.635447, 1.98896, 2.87979};
 
 // Überprüft, ob von allen Quellen Informationen vorliegen
 struct synchronizer{
@@ -51,11 +56,15 @@ ros::Publisher pub_kin_measure;
 // Message in der die gemessenen Kin-Größen gespeichert werden
 quadrotor_control::kinematics kin_measure_msg;
 
-// Zum Testen der callback_JointState Funktion mittels Gazebo (Plattform muss überbrückt werden)
-double VX = 0;
-double VY = 0;
-double VPhi = 0;
-double VTheta = 0;
+// für den Fall, dass der youBot die Pose nicht darstellen kann,
+// oder die Pose unsicher wird (Phi, Theta > 20°) werden die
+// kin_model-Daten direkt an die Regelung zurückgegeben
+quadrotor_control::kinematics kin_model_save_msg;
+
+bool safe = false;
+
+// Hier Berechnung w B -> N
+
 // ----------------------------------------------------------------------
 
 /*
@@ -63,6 +72,11 @@ double VTheta = 0;
 */
 void callback_odom( const nav_msgs::Odometry::Ptr& msg){
 	
+	if( safe == false ){
+		pub_kin_measure.publish( kin_model_save_msg );
+		return;
+	}
+
 	/*
 		Header header
 		string child_frame_id
@@ -75,7 +89,7 @@ void callback_odom( const nav_msgs::Odometry::Ptr& msg){
 
 	synch.base_ready = true;
 	if( synch.IMU_ready && synch.joint_ready ){
-		pub_kin_measure.publish( kin_measure_msg );
+		pub_kin_measure.publish( kin_model_save_msg );
 		synch.joint_ready = false;
 		synch.IMU_ready 	= false;
 		synch.base_ready 	= false;
@@ -84,19 +98,25 @@ void callback_odom( const nav_msgs::Odometry::Ptr& msg){
 
 /*
 	Wird aufgerufen, wenn Winkelinformationen (des Youbots) aktualisiert werden
-	Errechnet: Vx, Vy, Vz; PSI
+	Errechnet: Vz, PSI (Phi', Theta')
 */
 void callback_JointState( const sensor_msgs::JointState::Ptr& msg){
+
+	if( safe == false ){
+		pub_kin_measure.publish( kin_model_save_msg );
+		return;
+	}
+
 	MatrixXd Jacobi(6,5);	
 	
 	// Gelenkgeschwindigkeiten aus JointState übertragen
 	KDL::JntArray JointVels(5);
-	for(int i = 0; i <= 4; i++)		
+	for(int i = 0; i < 5; i++)		
 		JointVels(i) = msg->velocity[i];
 
 	// Gelenkpositionen aus JointState übertragen
 	KDL::JntArray Joints(5);
-	for(int i = 0; i <= 4; i++)		
+	for(int i = 0; i < 5; i++)		
 		Joints(i) = msg->position[i] - joint_offsets[i];
 
 	// Jacobi-Matrix aus Gelenkpositionen berechnen
@@ -107,32 +127,25 @@ void callback_JointState( const sensor_msgs::JointState::Ptr& msg){
 	Vels = Jacobi * JointVels.data; // [0..2] v, [3..5] w
 	
 	// Vels skalieren
-	Vels(0) /= scaleV;
-	Vels(1) /= scaleV;
+	Vels(0) /= scaleV;	// VX ~ 0
+	Vels(1) /= scaleV;	// VY ~ 0
 	Vels(2) /= scaleV;
 
 	// PSI berechnen
 	double psi = Joints(0);	
 	
 	// berechnete Werte in kin_measure_msg eintragen	
-	//kin_measure_msg.pose.position.x = 0;			// don't care
-	//kin_measure_msg.pose.position.y = 0;			// don't care
-	//kin_measure_msg.pose.position.z = 0;			// don't care
-	//kin_measure_msg.pose.orientation.x = 0;		// don't care
-	//kin_measure_msg.pose.orientation.y = 0;		// don't care
 	kin_measure_msg.pose.orientation.z = psi;
-	//kin_measure_msg.vel.linear.x = 0;				
-	//kin_measure_msg.vel.linear.y = 0;
-
-	kin_measure_msg.vel.linear.x = VX;	// NUR ZUM TESTEN VON CALLBACK_JOINTSTATE			
-	kin_measure_msg.vel.linear.y = VY;
 
 	kin_measure_msg.vel.linear.z = - Vels(2);
 	
-	// Winkelgeschwindigkeiten
-	kin_measure_msg.vel.angular.x = VPhi;
+	// Winkelgeschwindigkeiten (vielleicht nicht möglich aufgrund Rauschen)
+	// Drehung um X-Achse nicht möglich -> Modelldaten nehmen
+	kin_measure_msg.vel.angular.x = kin_model_save_msg.vel.angular.x;
 	kin_measure_msg.vel.angular.y = - Vels(4) /scaleR;
 	kin_measure_msg.vel.angular.z = - Vels(5);
+
+	// HIER ROTATION DER WINKELGESCHW. VON B -> N
 
 	ROS_INFO( "Measure:" );
 	ROS_INFO("PSI: % 06.4f, Vz: % 06.4f, VPhi: % 06.4f, VTheta: % 06.4f, VPsi: % 06.4f", 
@@ -155,11 +168,18 @@ void callback_JointState( const sensor_msgs::JointState::Ptr& msg){
 	Wird aufgerufen, wenn IMU neue Daten liefert
 */
 void callback_imu( const sensor_msgs::Imu::Ptr& msg){
-	// TODO: w einlesen (evtl. umrechnen?)
+
+	if( safe == false ){
+		pub_kin_measure.publish( kin_model_save_msg );
+		return;
+	}
+	
 	kin_measure_msg.vel.angular.x = msg->angular_velocity.x / scaleR;
 	kin_measure_msg.vel.angular.y = msg->angular_velocity.y / scaleR;
 	kin_measure_msg.vel.angular.z = msg->angular_velocity.z;
 	
+	// HIER ROTATION DER WINKELGESCHW. VON B -> N
+
 	synch.IMU_ready = true;
 	if( synch.joint_ready && synch.base_ready ){
 		pub_kin_measure.publish( kin_measure_msg );
@@ -169,40 +189,60 @@ void callback_imu( const sensor_msgs::Imu::Ptr& msg){
 	}	
 }
 
-void callback_kin_model( const quadrotor_control::kinematics::Ptr& msg ){
-	// VX und VY zwischenspeichern, falls später benötigt:
-	// wenn nur eine Achse getestet wird, muss die andere auch Werte liefern
-	VX = msg->vel.linear.x;
-	VY = msg->vel.linear.y;
-	VPhi = msg->vel.angular.x;
+/*
+	Wird aufgerufen, wenn neue Kinematik-Daten aus der Simulation vorhanden sind
+	-> Aufbereitung und Weitergabe an youBot
+*/
+void callback_kin_model( quadrotor_control::kinematics msg ){
+	
+	// Unschuldsbehauptung
+	safe = true;
+
+	// Kopiere die aktuellen Kinematics
+	// für den Fall safe == false -> direkte Rückführung)
+	std::memcpy( (void *)&kin_model_save_msg, (const void *)&msg, sizeof kin_model_save_msg );
 
 	// Scale: Vx, Vy, Vz
-	msg->vel.linear.x *= scaleV;
-	msg->vel.linear.y *= scaleV;
-	msg->vel.linear.z *= scaleV;
-	msg->pose.position.x *= scaleV;
-	msg->pose.position.y *= scaleV;
-	msg->pose.position.z *= scaleV;
+	msg.vel.linear.x *= scaleV;
+	msg.vel.linear.y *= scaleV;
+	msg.vel.linear.z *= scaleV;
+	msg.pose.position.x *= scaleV;
+	msg.pose.position.y *= scaleV;
+	msg.pose.position.z *= scaleV;
 
 	// Scale: Phi, Theta
-	msg->pose.orientation.x *= scaleR;
-	msg->pose.orientation.y *= scaleR;
+	msg.pose.orientation.x *= scaleR;
+	msg.pose.orientation.y *= scaleR;
 
 	// inverse Kinematik
 	// Aufteilung in X- und Y-Bewegung
 
 	double g[5];	// Gelenkwinkel
-	double z = - msg->pose.position.z;		// Negation!
+	double z = - msg.pose.position.z;		// Negation!
+
+	// Testen, ob übergebene Pose + Geschwindigkeiten sicher sind
   if( z < 0.5 ){
 		ROS_INFO("Unsichere Arbeitshoehe -> Bitte nach oben bewegen");
+		safe = false;
 		return;
 	}
+	if( abs(msg.pose.orientation.x) > 0.35 ){
+		ROS_INFO("Zu Steil! Rot(X)");
+		safe = false;
+		return;
+	}
+	if( abs(msg.pose.orientation.y) > 0.35 ){
+		ROS_INFO("Zu Steil! Rot(Y)");
+		safe = false;
+		return;
+	}
+	
 
 	// Rotation um Z-Achse
-	double psi = msg->pose.orientation.z;	
+	double psi = msg.pose.orientation.z;	
 
-	// Rotation um Y-Achse
-	double phi = M_PI/2 - msg->pose.orientation.y;	// NEGATION!
+	// Rotation um Y-Achse (phi aus inverser Kin.)
+	double phi = M_PI/2 - msg.pose.orientation.y;	// NEGATION!
 
 	// -----------   Inverse Kinematik   -------------------------
 
@@ -222,17 +262,27 @@ void callback_kin_model( const quadrotor_control::kinematics::Ptr& msg ){
 	g[3] = k_Arm * ( phi - M_PI/2 ) - g[1] - g[2];
 	g[4] = 0;
 	
+	// Min - Max überprüfen
+	for( int i = 0; i < 5; i++ ){
+		if( g[i] < joint_min_angles[i] || g[i] > joint_max_angles[i] ){
+			ROS_INFO("Pose nicht anfahrbar. Gelenk %i : %f", (i+1), g[i]);
+			safe = false;
+			return;
+		}
+	}
+
 	// joint_offsets addieren
-	for( int i = 0; i < 5; i++ )
+	for( int i = 0; i < 5; i++ ){
 		g[i] += joint_offsets[i];	
+	}
 
 	// -----------   Inverse Kinematik   ---------   ENDE  --------
 
 	brics_actuator::JointPositions msg_joints;
 	geometry_msgs::Twist msg_base;
 
-	msg_base.linear.x =   msg->vel.linear.x;
-	msg_base.linear.y = - msg->vel.linear.y;	// Transformation in Plattform-Koord
+	msg_base.linear.x =   msg.vel.linear.x;
+	msg_base.linear.y = - msg.vel.linear.y;	// Transformation in Plattform-Koord
 	msg_base.linear.z = 0.0f;
 
 	ROS_INFO( "BaseV: %f, %f", msg_base.linear.x, msg_base.linear.y );
@@ -248,8 +298,6 @@ void callback_kin_model( const quadrotor_control::kinematics::Ptr& msg ){
 		jv.joint_uri = joint_names[i];
 		jv.unit = "rad";
 		jv.value = g[i];
-		if( g[i] != g[i] ) // NaN
-			return;
 		msg_joints.positions.push_back(jv);
 	}        
 
